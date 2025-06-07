04c7a81483ad5473bdba9fadf6a358d7b770f9dd
Ulises <ucapistranmtz@gmail.com>
2025-06-07 16:06:47 -0500

    bubleSort

--- src/sort/bubbleSort/sort.ts
+++ src/sort/bubbleSort/sort.ts
@@ -1,7 +1,16 @@
 
 
-export function bubbleSort (arr:number[]) {
-console.log("Buble Sort");
+export function bubbleSort(arr: number[] = []) {
+    console.log("Buble Sort");
+    const n = arr.length;
+    let result: number[] = arr.slice(); // Or [...arr]
+     for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (result[j] > result[j + 1]) { // Only swap if left > right
+                [result[j], result[j + 1]] = [result[j + 1], result[j]];
+            }
+        }
+    }
 
-
+    return result;
 }
