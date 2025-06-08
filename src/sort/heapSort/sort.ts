2fdd704728db47b10c9d76f520bde95e01b60f2b
Ulises <ucapistranmtz@gmail.com>
2025-06-07 20:43:46 -0500

    Adding HeapSort

--- src/sort/heapSort/sort.ts
+++ src/sort/heapSort/sort.ts
@@ -1,37 +1,33 @@
 export function heapSort(arr: number[]): number[] {
+   
+    let result =arr.slice();
     let n = arr.length;
-    let midIndex = Math.floor(n / 2);
 
-    for (let i = midIndex - 1; i >= 0; i++) {
-        heapify(arr, n, i);// it means that i will move them as a heap
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(result, n, i);
     }
-
 
     for (let i = n - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapify(arr, i, 0);
+        [result[0], result[i]] = [result[i], result[0]];
+        heapify(result, i, 0);
     }
-    return arr;
+    return result;
 }
 
-
 function heapify(arr: number[], heapSize: number, rootIndex: number): void {
-
     let largest = rootIndex;
-    let left = 2 * rootIndex + 1
+    let left = 2 * rootIndex + 1;
     let right = 2 * rootIndex + 2;
 
     if (left < heapSize && arr[left] > arr[largest]) {
-        largest = left
+        largest = left;
     }
-
     if (right < heapSize && arr[right] > arr[largest]) {
         largest = right;
     }
 
     if (largest !== rootIndex) {
         [arr[rootIndex], arr[largest]] = [arr[largest], arr[rootIndex]];
-        heapify(arr, heapSize, largest)
+        heapify(arr, heapSize, largest);
     }
-
 }
